a29cccc8ab5106eaaed653b12960ee1c5456d899
Písek Pískovec <pisekpiskovec@gmail.com>
2026-02-18 22:28:11 +0100

    style: Remove debug info

--- src/tests.rs
+++ src/tests.rs
@@ -18,8 +18,7 @@ fn tst_add() {
     let mut cpu = ATmemory::init();
     let program: Vec<u8> = vec![0x00, 0xE1, 0x13, 0xE0, 0x01, 0x0F];
     cpu.load_flash_from_vec(program.clone()).ok();
-    for (idx, _) in program.iter().enumerate() {
-        println!("{}", idx);
+    for _ in program.iter().enumerate() {
         cpu.step().ok();
     }
     assert_eq!(cpu.registers()[16], 19)
@@ -31,8 +30,7 @@ fn tst_sub() {
     let mut cpu = ATmemory::init();
     let program: Vec<u8> = vec![0x01, 0xE8, 0x15, 0xE0, 0x01, 0x1B];
     cpu.load_flash_from_vec(program.clone()).ok();
-    for (idx, _) in program.iter().enumerate() {
-        println!("{}", idx);
+    for _ in program.iter().enumerate() {
         cpu.step().ok();
     }
     assert_eq!(cpu.registers()[16], 124)
