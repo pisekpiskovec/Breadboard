6b37b580d542910f85d96ec4f9f5b31f790b17cd
Písek Pískovec <pisekpiskovec@gmail.com>
2026-04-11 22:01:18 +0200

    refactor: Move app into own module

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,7 @@ mod tests;
 mod gui;
 
 #[cfg(feature = "tui")]
-#[path = "tui/flash.rs"]
-mod flash;
-
-#[cfg(feature = "tui")]
-#[path = "tui/status.rs"]
-mod status;
+mod tui;
 
 fn main() {
     #[cfg(all(feature = "gui", feature = "tui"))]
@@ -34,24 +29,8 @@ fn main() {
 
     #[cfg(feature = "tui")]
     {
-        use crate::memory::ATmemory;
-        let mut cpu = ATmemory::init();
-        cpu.load_hex("/home/pisek/Projekty/Rust/Breadboard/tests/rcall-tst/test.hex").ok();
-
-        use crate::config::Config;
-        let config = Config::load().ok().unwrap();
-
-        use appcui::system::App;
-        let mut app = App::new().title("Breadboard").build().unwrap();
-
-        use flash::FlashWindow;
-        let flash_window = FlashWindow::new(config, cpu);
-
-        use status::StatusWindow;
-        let status_window = StatusWindow::new(cpu);
-
-        app.add_window(flash_window);
-        app.add_window(status_window);
+        use crate::tui::TUInterface;
+        let app = TUInterface::init();
         app.run();
     }
 }
--- src/tui.rs
+++ src/tui.rs
@@ -1,2 +1,29 @@
+use crate::{config::Config, memory::ATmemory, tui::{flash::FlashWindow, status::StatusWindow}};
+
 mod flash;
 mod status;
+
+pub struct TUInterface {
+    cpu: ATmemory,
+    config: Config,
+}
+
+impl TUInterface {
+    pub fn new() -> Self {
+        let config = Config::load().unwrap_or_default();
+        let mut cpu = ATmemory::init();
+        cpu.connect_to_hw(&config.bridge_address).ok();
+
+        Self { cpu, config }
+    }
+
+    pub fn init() -> appcui::prelude::App {
+        let mut app = appcui::system::App::new().title("Breadboard").build().unwrap();
+        let mut interface = Self::new();
+
+        app.add_window(FlashWindow::new(interface.config, interface.cpu));
+        app.add_window(StatusWindow::new(interface.cpu));
+
+        app
+    }
+}
