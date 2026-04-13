263c07f1a458c9b416245592f53df92b4e1c34c0
Písek Pískovec <pisekpiskovec@gmail.com>
2026-04-13 22:36:30 +0200

    fix(tui): Make labels not overlap and fix win size

--- src/tui/status.rs
+++ src/tui/status.rs
@@ -3,8 +3,7 @@ use std::{cell::RefCell, rc::Rc};
 use appcui::prelude::{Label, Window};
 
 #[Window]
-pub struct StatusWindow {
-}
+pub struct StatusWindow {}
 
 impl StatusWindow {
     pub fn new(cpu: Rc<RefCell<crate::memory::ATmemory>>) -> Self {
@@ -16,43 +15,31 @@ impl StatusWindow {
         let zp = cpu_ref.zp();
         let cycle_cnt = cpu_ref.cycle_cnt();
         let mut win = Self {
-            base: window!("'Status',a:c,w:10,h:30,flags:sizeable"),
+            base: window!("'Status',a:c,w:32,h:8"),
         };
         win.add(Label::new(
             format!("Program Counter | {:#08X}", pc).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(0).width(32).build(),
         ));
         win.add(Label::new(
             format!("Stack Pointer | {:#06X}", sp).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(1).width(32).build(),
         ));
         win.add(Label::new(
             format!("X Pointer | {:#06X}", xp).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(2).width(32).build(),
         ));
         win.add(Label::new(
             format!("Y Pointer | {:#06X}", yp).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(3).width(32).build(),
         ));
         win.add(Label::new(
             format!("Z Pointer | {:#06X}", zp).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(4).width(32).build(),
         ));
         win.add(Label::new(
             format!("Cycle Counter | {:06}", cycle_cnt).as_str(),
-            LayoutBuilder::new()
-                .alignment(Alignment::CenterLeft)
-                .build(),
+            LayoutBuilder::new().x(0).y(5).width(32).build(),
         ));
         win
     }
