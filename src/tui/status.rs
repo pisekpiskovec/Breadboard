7f1874b09cec2013d68855330b34863d19dd65a5
Písek Pískovec <pisekpiskovec@gmail.com>
2026-05-01 21:03:28 +0200

    feat: Display current instruction in status win

--- src/tui/status.rs
+++ src/tui/status.rs
@@ -1,6 +1,9 @@
 use std::{cell::RefCell, rc::Rc, time::Duration};
 
-use appcui::prelude::{Label, Window};
+use appcui::{
+    prelude::{Label, Window},
+    ui::hline::Flags,
+};
 
 #[Window(events=[WindowEvents, TimerEvents])]
 pub struct StatusWindow {
@@ -11,12 +14,13 @@ pub struct StatusWindow {
     xp_lb: Handle<Label>,
     yp_lb: Handle<Label>,
     zp_lb: Handle<Label>,
+    inst_lb: Handle<Label>,
 }
 
 impl StatusWindow {
     pub fn new(cpu: Rc<RefCell<crate::memory::ATmemory>>) -> Self {
         let mut win = Self {
-            base: window!("'Status',x:0,y:1,w:32,h:8,flags:NoCloseButton"),
+            base: window!("'Status',x:0,y:1,w:32,h:10,flags:NoCloseButton"),
             cc_lb: Handle::None,
             cpu,
             pc_lb: Handle::None,
@@ -24,6 +28,7 @@ impl StatusWindow {
             xp_lb: Handle::None,
             yp_lb: Handle::None,
             zp_lb: Handle::None,
+            inst_lb: Handle::None,
         };
         win.set_hotkey(key!("Alt+S"));
         win.pc_lb = win.add(Label::new(
@@ -49,6 +54,15 @@ impl StatusWindow {
         win.cc_lb = win.add(Label::new(
             "",
             LayoutBuilder::new().x(0).y(5).width(32).build(),
+        ));
+        win.add(HLine::new(
+            "",
+            LayoutBuilder::new().x(0).y(6).width(32).build(),
+            Flags::None,
+        ));
+        win.inst_lb = win.add(Label::new(
+            "",
+            LayoutBuilder::new().x(0).y(7).width(32).build(),
         ));
 
         if let Some(timer) = win.timer() {
@@ -99,6 +113,16 @@ impl TimerEvents for StatusWindow {
 
         let text = format!("Cycle Counter | {:06}", self.cpu.borrow().cycle_cnt());
         let h = self.cc_lb;
+        if let Some(lb) = self.control_mut(h) {
+            lb.set_caption(&text);
+        }
+
+        let mut text = format!("Current Instruction | {}", self.cpu.borrow().get_instruction());
+        if let (Some(start), Some(end)) = (text.find('{'), text.find('}')) {
+            let result = format!("{}{}", &text[..start], &text[end+1..]);
+            text = result.trim_ascii().to_string();
+        };
+        let h = self.inst_lb;
         if let Some(lb) = self.control_mut(h) {
             lb.set_caption(&text);
         }
